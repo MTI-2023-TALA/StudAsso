16b80b9d260fcf1a5a657b26242a1b377483600f
Alois Heloir <alois.heloir@live.fr>
2022-03-17 19:54:33 +0100

    add lib login

---
+++ libs/frontend/login/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/frontend-login.module';
+export * from './lib/login-page/login-page.component';
---
+++ libs/frontend/login/src/lib/frontend-login.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [LoginPageComponent],
+})
+export class FrontendLoginModule {}
