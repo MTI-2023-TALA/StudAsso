890d40a2fe24d258ea56f168f581d0c104e39797
Alois Heloir <alois.heloir@live.fr>
2022-03-17 19:54:39 +0100

    add lib login

--- libs/frontend/login/src/lib/login-page/login-page.component.ts
+++ libs/frontend/login/src/lib/login-page/login-page.component.ts
@@ -3,13 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'stud-asso-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class LoginPageComponent implements OnInit {}
