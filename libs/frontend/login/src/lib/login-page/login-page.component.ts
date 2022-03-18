52267f2bef352f3a8c9996f403c137c7bbedae1e
Alois Heloir <alois.heloir@live.fr>
2022-03-18 12:42:42 +0100

    fix ci

--- libs/frontend/login/src/lib/login-page/login-page.component.ts
+++ libs/frontend/login/src/lib/login-page/login-page.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'stud-asso-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent implements OnInit {}
+export class LoginPageComponent {}
