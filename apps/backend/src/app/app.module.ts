c2fe242fc5046c29a320819c75496f11a7a73e81
Loup Dallier <loup.dallier@epita.fr>
2022-03-19 19:26:50 +0100

    establish connection to db

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionOptions } from 'typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), { autoLoadEntities: true }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
