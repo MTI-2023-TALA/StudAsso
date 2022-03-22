6f938ed13a4cf01f22d0bbefdf383437548aebdb
Loup Dallier <loup.dallier@epita.fr>
2022-03-22 16:14:38 +0100

    change @dtos path

--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto, UpdateUserDto } from '@stud-asso/shared/dtos';
 
 @Controller('users')
 export class UsersController {
--- libs/shared/dtos/src/index.ts
+++ libs/shared/dtos/src/index.ts
@@ -1,1 +1,8 @@
 export * from './lib/shared-dtos.module';
+
+export * from './lib/association/create-association.dto';
+export * from './lib/association/update-association.dto';
+export * from './lib/associations-member/create-associations-member.dto';
+export * from './lib/associations-member/update-associations-member.dto';
+export * from './lib/user/create-user.dto';
+export * from './lib/user/update-user.dto';
