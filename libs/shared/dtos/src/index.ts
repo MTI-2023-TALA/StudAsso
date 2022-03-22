b25c6237671f6bc0186d53c6906408f99841a278
Théo Casner <theo.casner@epita.fr>
2022-03-22 20:12:22 +0100

    add(fix merge, fixed path): fix

--- libs/shared/dtos/src/index.ts
+++ libs/shared/dtos/src/index.ts
@@ -7,3 +7,4 @@ export * from './lib/associations-member/create-associations-member.dto';
 export * from './lib/associations-member/update-associations-member.dto';
 export * from './lib/user/create-user.dto';
 export * from './lib/user/update-user.dto';
+export * from './lib/user/user.dto';
