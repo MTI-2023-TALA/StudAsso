53970597fa75339fe4a87de773f6df2b7cb33763
Alois <alois.heloir@live.fr>
2022-03-25 14:23:24 +0100

    [Frontend][Lib][Navbar] Rework css and html of the navbar (#12)
    * Rework css and html of the navbar
    
    * Add new func
    
    * Clean code

--- apps/frontend/school/src/app/app-routing.module.ts
+++ apps/frontend/school/src/app/app-routing.module.ts
@@ -25,7 +25,6 @@ const routes: Routes = [
         path: 'stats',
         component: ModalCreateAssociationComponent,
         data: { title: 'Portail Ecole' },
-        canActivate: [IsSignGuard],
       },
     ],
   },
