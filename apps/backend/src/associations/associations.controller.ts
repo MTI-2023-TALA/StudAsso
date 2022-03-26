ab91b99b43dfc60eb035431b1eb855129f9102de
Loup Dallier <loup.dallier@epita.fr>
2022-03-26 16:13:56 +0100

    schema validation for endpoints

--- apps/backend/src/associations/associations.controller.ts
+++ apps/backend/src/associations/associations.controller.ts
@@ -1,12 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { AssociationsService } from './associations.service';
-import { BaseController } from '../base/base.controller';
-import { Association } from './entities/association.entity';
-
+import {
+  CreateAssociationDto,
+  UpdateAssociationDto,
+} from '@stud-asso/shared/dtos';
 
 @Controller('associations')
-export class AssociationsController extends BaseController<Association> {
-  constructor(private readonly associationsService: AssociationsService) {
-    super(associationsService);
+export class AssociationsController {
+  constructor(private readonly associationsService: AssociationsService) {}
+
+  @Post()
+  create(@Body() createAssociationDto: CreateAssociationDto) {
+    return this.associationsService.create(createAssociationDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.associationsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.associationsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateAssociationDto: UpdateAssociationDto
+  ) {
+    return this.associationsService.update(+id, updateAssociationDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.associationsService.remove(+id);
   }
 }
