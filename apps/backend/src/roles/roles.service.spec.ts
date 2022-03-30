ad281bb5eb5b18f14fb783820add162baff1fd7e
Théo Casner <theo.casner@epita.fr>
2022-03-30 22:33:33 +0200

    add(test ci): fix test

--- apps/backend/src/roles/roles.service.spec.ts
+++ apps/backend/src/roles/roles.service.spec.ts
@@ -1,13 +1,22 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { TestingModule } from '@nestjs/testing';
+import { CreateMockRepo } from '../../helpers/generic-spec/generic-service-spec';
+import { Role } from './entities/role.entity';
 import { RolesService } from './roles.service';
 
 describe('RolesService', () => {
   let service: RolesService;
 
+  const mockRepo = {
+    // TODO set mock value
+    find: jest.fn().mockResolvedValue(42),
+  };
+
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [RolesService],
-    }).compile();
+    const module: TestingModule = await CreateMockRepo(
+      RolesService,
+      Role,
+      mockRepo
+    );
 
     service = module.get<RolesService>(RolesService);
   });
