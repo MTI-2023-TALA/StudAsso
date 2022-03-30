84d6788c4995eeae22515287226fc67b6dd4b7e9
Alois Heloir <alois.heloir@live.fr>
2022-03-30 09:48:32 +0200

    Add table component

--- libs/frontend/table/src/lib/frontend-table.module.ts
+++ libs/frontend/table/src/lib/frontend-table.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableHeaderComponent } from './table-header/table-header.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [TableHeaderComponent],
-  exports: [TableHeaderComponent],
+  declarations: [TableHeaderComponent, TableComponent],
+  exports: [TableHeaderComponent, TableComponent],
 })
 export class FrontendTableModule {}
