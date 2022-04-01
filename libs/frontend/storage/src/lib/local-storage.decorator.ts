568103f3d17df1b2d65e375e243842621e7063da
Alois <alois.heloir@live.fr>
2022-04-01 13:14:00 +0200

    [Frontend][Lib][Storage] Fix bug (#27)
    * Fix bug
    
    * Fix bug

--- libs/frontend/storage/src/lib/local-storage.decorator.ts
+++ libs/frontend/storage/src/lib/local-storage.decorator.ts
@@ -19,8 +19,9 @@ export function UseStorage(storageKey: string) {
       if (firstime && valStorage) {
         val = JSON.parse(valStorage);
         firstime = false;
+        return;
       }
-      setData(storageKey, newVal);
+      setData(storageKey, val);
     };
 
     Object.defineProperty(target, key, {
@@ -52,6 +53,7 @@ export function UseStorageUnoptimized(storageKey: string) {
       if (firstime && valStorage) {
         val = JSON.parse(valStorage);
         firstime = false;
+        return;
       }
       setData(storageKey, val);
     };
