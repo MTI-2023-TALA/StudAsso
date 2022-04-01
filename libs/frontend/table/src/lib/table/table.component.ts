d2b58cfe137c4976604022249588b30e721f23fc
Alois Heloir <alois.heloir@live.fr>
2022-04-01 22:51:42 +0200

    Add stylelint for every lib with has scss

--- libs/frontend/table/src/lib/table/table.component.ts
+++ libs/frontend/table/src/lib/table/table.component.ts
@@ -4,7 +4,6 @@ import { TableHeaderItem } from '../table-header/table-header.model';
 @Component({
   selector: 'stud-asso-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss'],
 })
 export class TableComponent {
   @Input() headerList: TableHeaderItem[] = [];
