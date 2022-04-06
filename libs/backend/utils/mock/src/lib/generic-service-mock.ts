6f11d3bd09a46b508def6f1674bb931fc78d0196
Loup Dallier <loup.dallier@epita.fr>
2022-04-06 17:44:11 +0200

    udpate service mock

--- libs/backend/utils/mock/src/lib/generic-service-mock.ts
+++ libs/backend/utils/mock/src/lib/generic-service-mock.ts
@@ -1,9 +1,10 @@
 import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Base } from '@stud-asso/backend/utils/base';
+import { BaseDto } from '@stud-asso/shared/dtos';
 
 // TODO: change type of mockRepo
-export const CreateMockRepo = (service: any, entity: typeof Base, mockRepo: any) =>
+export const CreateMockRepo = (service: any, entity: typeof BaseDto, mockRepo: any) =>
   Test.createTestingModule({
     providers: [service, { provide: getRepositoryToken(entity), useValue: mockRepo }],
   }).compile();
