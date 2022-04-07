61907151ffdce1a63598e4b32d328e8b6fb5aa40
Alan Schaeffer <alan.schaeffer29@gmail.com>
2022-04-07 10:50:49 +0200

    Added html table for members with new generic table

--- libs/frontend/feature/asso/members-list/src/lib/members-list/members-list.component.ts
+++ libs/frontend/feature/asso/members-list/src/lib/members-list/members-list.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { TableHeaderItem } from '@stud-asso/frontend-shared-table';
+import { ModalService } from '@stud-asso/frontend-shared-modal';
+import { TableConfiguration } from '@stud-asso/frontend-shared-table';
+import { ToastService, ToastType } from '@stud-asso/frontend-shared-toast';
+
+enum Action {
+  EDIT = 1,
+  DELETE = 2,
+}
 
 @Component({
   selector: 'stud-asso-members-list',
@@ -7,26 +14,29 @@ import { TableHeaderItem } from '@stud-asso/frontend-shared-table';
   styleUrls: ['./members-list.component.scss'],
 })
 export class MembersListComponent {
-  tableHeader: TableHeaderItem[] = [
-    {
-      label: 'Avatar',
-      size: 1,
-    },
-    {
-      label: 'Login',
-      size: 3,
-    },
-    {
-      label: 'Rôle(s)',
-      size: 3,
-    },
-    {
-      label: 'Modifier',
-      size: 2,
-    },
-  ];
+  tableConfiguration: TableConfiguration = {
+    columns: [
+      { title: 'Avatar', size: 1, dataProperty: 'name' },
+      { title: 'Login', size: 3, dataProperty: 'name' },
+      { title: 'Rôle(s)', size: 2, dataProperty: 'name' },
+    ],
+    actions: [
+      { label: 'Modifier', action: Action.EDIT, dataProperty: 'id' },
+      {
+        label: 'Supprimer',
+        action: Action.DELETE,
+        dataProperty: 'id',
+      },
+    ],
+  };
 
-  constructor() {
+  membersList: any[] = [];
+
+  constructor(private modal: ModalService, private toast: ToastService) {
     return;
+  }
+
+  handleError() {
+    return () => this.toast.addAlert({ title: 'Erreur lors de la récupération des membres', type: ToastType.Error });
   }
 }
