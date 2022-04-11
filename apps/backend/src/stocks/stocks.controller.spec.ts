f8d440c5d2de84532bef963011677752222b9e43
Théo Casner <theo.casner@epita.fr>
2022-04-11 15:28:24 +0200

    add(stocks): fake test to pass CI

--- apps/backend/src/stocks/stocks.controller.spec.ts
+++ apps/backend/src/stocks/stocks.controller.spec.ts
@@ -1,15 +1,17 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { TestingModule } from '@nestjs/testing';
 import { StocksController } from './stocks.controller';
 import { StocksService } from './stocks.service';
+import { CreateMockService } from '@stud-asso/backend/utils/mock';
 
 describe('StocksController', () => {
   let controller: StocksController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [StocksController],
-      providers: [StocksService],
-    }).compile();
+    const module: TestingModule = await CreateMockService([StocksController], {
+      type: StocksService,
+      // TODO: set mock value
+      methods: { findAll: jest.fn().mockResolvedValue([42]) },
+    });
 
     controller = module.get<StocksController>(StocksController);
   });
--- apps/backend/src/stocks/stocks.service.spec.ts
+++ apps/backend/src/stocks/stocks.service.spec.ts
@@ -1,13 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateMockRepo } from '@stud-asso/backend/utils/mock';
+import { Stock } from './entities/stock.entity';
 import { StocksService } from './stocks.service';
 
 describe('StocksService', () => {
   let service: StocksService;
 
+  const mockRepo = {
+    // TODO set mock value
+    find: jest.fn().mockResolvedValue(42),
+  };
+
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [StocksService],
-    }).compile();
+    const module: TestingModule = await CreateMockRepo(StocksService, Stock, mockRepo);
 
     service = module.get<StocksService>(StocksService);
   });
