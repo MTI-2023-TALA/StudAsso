1f9f1c072bc2d3fddc7c85f74b4df483a1a48384
Théo Casner <theo.casner@epita.fr>
2022-04-12 14:37:03 +0200

    add(events): entitty

---
+++ apps/backend/src/events/entities/event.entity.ts
@@ -0,0 +1,26 @@
+import { Base } from '@stud-asso/backend/utils/base';
+import { EventDto } from '@stud-asso/shared/dtos';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('events')
+export class Event extends Base {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'varchar' })
+  name: string;
+
+  @Column({ type: 'timestamp' })
+  date: Date;
+
+  @Column({ type: 'text' })
+  content: string;
+
+  @Column({ type: 'int', name: 'association_id' })
+  associationId: number;
+
+  constructor(dto?: EventDto) {
+    super();
+    if (dto) Object.assign(this, dto);
+  }
+}
