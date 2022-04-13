bc50af4cc148978eb64a96984a379f56fa66c4ac
Alois Heloir <alois.heloir@live.fr>
2022-04-13 16:29:14 +0200

    Remove create-association

--- apps/frontend/school/src/app/app.module.ts
+++ apps/frontend/school/src/app/app.module.ts
@@ -2,21 +2,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { FrontendCoreApiModule } from '@stud-asso/frontend-core-api';
-import { FrontendFeatureSchoolCreateAssociationModule } from '@stud-asso/frontend-feature-school-create-association';
 import { FrontendSharedTableModule } from '@stud-asso/frontend-shared-table';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FrontendCoreApiModule,
-    RouterModule,
-    FrontendFeatureSchoolCreateAssociationModule,
-    FrontendSharedTableModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, FrontendCoreApiModule, RouterModule, FrontendSharedTableModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
