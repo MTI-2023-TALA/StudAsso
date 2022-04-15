1fc6af86f95252c479dfc3a8d798dcc8cfc3fdd2
Alois Heloir <alois.heloir@live.fr>
2022-04-15 14:48:59 +0200

    Fix formly not working

--- apps/frontend/school/src/app/app.module.ts
+++ apps/frontend/school/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { FrontendCoreApiModule } from '@stud-asso/frontend-core-api';
+import { FrontendSharedFormlyModule } from '@stud-asso/frontend-shared-formly';
 import { FrontendSharedTableModule } from '@stud-asso/frontend-shared-table';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FrontendCoreApiModule, RouterModule, FrontendSharedTableModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FrontendCoreApiModule,
+    RouterModule,
+    FrontendSharedTableModule,
+    FrontendSharedFormlyModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
