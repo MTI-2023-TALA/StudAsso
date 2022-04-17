cf83750e6acf0104ab62febb8c3771fe9acfdea2
Alois Heloir <alois.heloir@live.fr>
2022-04-17 10:16:58 +0200

    Remove useless module linking

--- libs/frontend/shared/toast/src/lib/frontend-shared-toast.module.ts
+++ libs/frontend/shared/toast/src/lib/frontend-shared-toast.module.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { FrontendSharedTableModule } from '@stud-asso/frontend-shared-table';
 import { NgModule } from '@angular/core';
 import { ToastComponent } from './toast/toast.component';
 import { ToastDirective } from './toast.directive';
 
 @NgModule({
-  imports: [CommonModule, FrontendSharedTableModule],
+  imports: [CommonModule],
   declarations: [ToastComponent, ToastDirective],
   exports: [ToastDirective],
 })
