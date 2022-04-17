d61d8c19bfd6477e79ae0228d7cb1b4d37cf4deb
Alois <alois.heloir@live.fr>
2022-04-17 10:20:59 +0200

    [Frontend] Lib Auth: Swap login-page to it own lib (#55)
    * Swap login-page to it own lib
    
    * Add tag
    
    * Remove useless module linking

--- apps/frontend/student/src/app/app-routing.module.ts
+++ apps/frontend/student/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
-import { IsNotSignGuard, IsSignGuard, LoginPageComponent } from '@stud-asso/frontend-core-auth';
+import { IsNotSignGuard, IsSignGuard } from '@stud-asso/frontend-core-auth';
 import { RouterModule, Routes } from '@angular/router';
 
+import { LoginPageComponent } from '@stud-asso/frontend/shared/login-page';
 import { MainRoutingComponent } from '@stud-asso/frontend-shared-main-routing-component';
 import { NavbarItem } from '@stud-asso/frontend-shared-navbar';
 import { NgModule } from '@angular/core';
