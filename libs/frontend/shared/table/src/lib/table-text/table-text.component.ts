2d4095b57939026f586a4b619b1d785423a3f1c1
Alois Heloir <alois.heloir@live.fr>
2022-04-18 18:03:53 +0200

    Redesign table

--- libs/frontend/shared/table/src/lib/table-text/table-text.component.ts
+++ libs/frontend/shared/table/src/lib/table-text/table-text.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'stud-asso-table-text',
+  styleUrls: ['./table-text.component.scss'],
   templateUrl: './table-text.component.html',
 })
 export class TableTextComponent {
