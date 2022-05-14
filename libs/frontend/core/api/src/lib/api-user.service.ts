feb981c9074a597b61825bc3c4b92959990a8679
Alan Schaeffer <86117558+AlanasSchaeffer@users.noreply.github.com>
2022-05-14 19:15:22 +0200

    [Frontend] Association: Added president dropdown on new association (#79)
    * prepare change to add association president
    
    * added api call to add president of an association
    
    * modify update formly

---
+++ libs/frontend/core/api/src/lib/api-user.service.ts
@@ -0,0 +1,21 @@
+import { CreateUserDto, UpdateUserDto, UserIdAndEmailDto } from '@stud-asso/shared/dtos';
+
+import { ApiGenericService } from './api-generic.service';
+import { ApiService } from './api.service';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiUserService extends ApiGenericService<CreateUserDto, UpdateUserDto> {
+  private api: ApiService;
+  constructor(apiService: ApiService) {
+    super(apiService);
+    this.api = apiService;
+    this.url = 'users';
+  }
+
+  getIdAndEmail() {
+    return this.api.get<UserIdAndEmailDto[]>(`${this.url}/idandemail`);
+  }
+}
