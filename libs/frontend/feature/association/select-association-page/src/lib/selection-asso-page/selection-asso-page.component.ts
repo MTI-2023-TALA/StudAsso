31cb0d4607b6bf448becc4a28bef890db1c1b12c
Alois Heloir <alois.heloir@live.fr>
2022-05-17 10:39:14 +0200

    rework(html/css): Beautiful page of select-asso-page

--- libs/frontend/feature/association/select-association-page/src/lib/selection-asso-page/selection-asso-page.component.ts
+++ libs/frontend/feature/association/select-association-page/src/lib/selection-asso-page/selection-asso-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { ApiUserService } from '@stud-asso/frontend-core-api';
 import { AssoUserDto } from '@stud-asso/shared/dtos';
+import { AuthService } from '@stud-asso/frontend-core-auth';
 import { Router } from '@angular/router';
 import { setData } from '@stud-asso/frontend-core-storage';
 
@@ -11,18 +12,22 @@ import { setData } from '@stud-asso/frontend-core-storage';
   styleUrls: ['./selection-asso-page.component.scss'],
 })
 export class SelectionAssoPageComponent implements OnInit {
-  constructor(private router: Router, private api: ApiUserService) {}
-
   assoList: AssoUserDto | null;
+
+  constructor(private router: Router, private api: ApiUserService, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.api.getUserAsso().subscribe((assoList) => {
+      this.assoList = assoList;
+    });
+  }
 
   onClickAsso(id: number) {
     setData('asso-id', id);
     this.router.navigate(['/']);
   }
 
-  ngOnInit() {
-    this.api.getUserAsso().subscribe((assoList) => {
-      this.assoList = assoList;
-    });
+  logout() {
+    this.authService.logout();
   }
 }
