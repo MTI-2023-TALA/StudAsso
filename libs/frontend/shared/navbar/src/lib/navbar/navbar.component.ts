6287d496edd5621097deecbbc1bc31baed19d0be
Alois <alois.heloir@live.fr>
2022-05-17 11:02:05 +0200

    [Frontend] Rework: select asso page (#88)
    * rework(html/css): Beautiful page of select-asso-page
    
    * feat(assoName): Show asso name when login into an asso
    
    * fix(unuse import)
    
    * fix(typo)

--- libs/frontend/shared/navbar/src/lib/navbar/navbar.component.ts
+++ libs/frontend/shared/navbar/src/lib/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from 
 
 import { AuthService } from '@stud-asso/frontend-core-auth';
 import { NavbarItem } from './navbar.model';
+import { getData } from '@stud-asso/frontend-core-storage';
 
 @Component({
   selector: 'stud-asso-navbar',
@@ -15,8 +16,13 @@ export class NavbarComponent {
 
   @Input() shouldShowLargeNavbar = true;
   @Output() shouldShowLargeNavbarChange = new EventEmitter<boolean>();
+  assoName: string | null;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {
+    if (getData('asso-name')) {
+      this.assoName = JSON.parse(getData('asso-name') as string);
+    }
+  }
 
   toggleShowLargeNavbar() {
     this.shouldShowLargeNavbar = !this.shouldShowLargeNavbar;
