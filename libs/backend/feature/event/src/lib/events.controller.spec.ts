d4340958fc4544944fee3a150ad5cd023a6179a4
Théo Casner <38163392+TheSalfy@users.noreply.github.com>
2022-05-20 20:47:20 +0200

    [Backend](Tests) added tests for events (#89)
    * add(event): added tests for events controller
    
    * add(event): added tests for event service
    
    * add(event): review PR - added constants
    
    * add(associations test): review PR - added constants

--- libs/backend/feature/event/src/lib/events.controller.spec.ts
+++ libs/backend/feature/event/src/lib/events.controller.spec.ts
@@ -1,10 +1,31 @@
+import { EventDto, UpdateEventDto } from '@stud-asso/shared/dtos';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
+import { UpdateResult } from 'typeorm';
+import { plainToInstance } from 'class-transformer';
+
+const mockCreateEventDto: EventDto = {
+  id: 1,
+  name: 'Event1',
+  date: new Date('2022-01-15'),
+  content: 'An amazing description',
+  associationId: 1,
+};
+const mockfindAllEvents: EventDto[] = [
+  { id: 1, name: 'Event1', date: new Date('2022-01-15'), content: 'An amazing description', associationId: 1 },
+  { id: 2, name: 'Event2', date: new Date('2022-02-15'), content: 'An amazing description again', associationId: 2 },
+];
+const mockedUpdateResult: UpdateResult = {
+  raw: [],
+  generatedMaps: [],
+  affected: 1,
+};
 
 describe('EventsController', () => {
   let controller: EventsController;
+  let service: EventsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,15 +33,63 @@ describe('EventsController', () => {
       providers: [
         {
           provide: EventsService,
-          useValue: jest.fn(),
+          useValue: {
+            create: jest.fn(() => Promise.resolve(mockCreateEventDto)),
+            findAll: jest.fn(() => Promise.resolve(mockfindAllEvents)),
+            findOne: jest.fn(() => Promise.resolve(mockfindAllEvents[0])),
+            update: jest.fn(() => Promise.resolve(mockedUpdateResult)),
+            delete: jest.fn(() => Promise.resolve(mockedUpdateResult)),
+          },
         },
       ],
     }).compile();
 
-    controller = module.get<EventsController>(EventsController);
+    controller = await module.get<EventsController>(EventsController);
+    service = await module.get<EventsService>(EventsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  afterEach(() => jest.clearAllMocks());
+
+  describe('createEvent', () => {
+    it('should call eventsService.create', async () => {
+      const create = jest.spyOn(service, 'create');
+      const eventParams = {
+        name: 'Event1',
+        date: new Date('2022-01-15'),
+        content: 'An amazing description',
+        associationId: 1,
+      };
+
+      const createdEvent = await controller.create(eventParams);
+      expect(createdEvent).toEqual(mockCreateEventDto);
+
+      expect(create).toHaveBeenCalledTimes(1);
+      expect(create).toHaveBeenCalledWith(eventParams);
+    });
+  });
+
+  describe('findAllEvents', () => {
+    it('should call eventsService.findAll', async () => {
+      expect(await controller.findAll()).toEqual(mockfindAllEvents);
+    });
+  });
+
+  describe('findOneEvent', () => {
+    it('shoud call eventsService.findOne', async () => {
+      expect(await controller.findOne('1')).toEqual(mockfindAllEvents[0]);
+    });
+  });
+
+  describe('updateEvent', () => {
+    it('should call eventsService.update', async () => {
+      const updateEventDto = plainToInstance(UpdateEventDto, { content: 'Event1 Renamed' });
+      expect(await controller.update('1', updateEventDto)).toEqual(mockedUpdateResult);
+    });
+  });
+
+  describe('deleteEvent', () => {
+    it('should call eventsService.delete', async () => {
+      expect(await controller.delete('1')).toEqual(mockedUpdateResult);
+    });
   });
 });
