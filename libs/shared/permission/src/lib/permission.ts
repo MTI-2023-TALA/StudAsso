922cd167b575648e065cc62fb078e639d17faf6f
Alois Heloir <alois.heloir@live.fr>
2022-05-29 14:49:28 +0200

    fix(typo): Premission

--- libs/shared/permission/src/lib/permission.ts
+++ libs/shared/permission/src/lib/permission.ts
@@ -1,6 +1,6 @@
 import { PermissionColor } from './color';
 
-export enum PremissionId {
+export enum PermissionId {
   STOCK_MANAGEMENT = 'STOCK_MANAGEMENT',
   STOCK_READ = 'STOCK_READ',
 }
@@ -12,19 +12,19 @@ export interface IPermission {
   description: string;
 }
 
-interface Premissions {
+interface Permissions {
   [key: string]: IPermission;
 }
 
-export const permissions: Premissions = {
+export const permissions: Permissions = {
   STOCK_MANAGEMENT: {
-    id: PremissionId.STOCK_MANAGEMENT,
+    id: PermissionId.STOCK_MANAGEMENT,
     name: 'Stock Management',
     color: PermissionColor.PRIMARY,
     description: 'Stock Management',
   },
   STOCK_READ: {
-    id: PremissionId.STOCK_READ,
+    id: PermissionId.STOCK_READ,
     name: 'Stock Read',
     color: PermissionColor.SECONDARY,
     description: 'Stock Read',
