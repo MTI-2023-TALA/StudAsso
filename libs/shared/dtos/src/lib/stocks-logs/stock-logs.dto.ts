6fc1b629b7dacb8965c887e226c6bf4c857e6c72
Théo Casner <theo.casner@epita.fr>
2022-06-13 10:52:41 +0200

    add(dto): created dto

--- libs/shared/dtos/src/lib/stocks-logs/stock-logs.dto.ts
+++ libs/shared/dtos/src/lib/stocks-logs/stock-logs.dto.ts
@@ -7,3 +7,21 @@ export class StockLogsDto {
   action: string;
   createdAt: Date;
 }
+
+export class StockLogsWithUserDto {
+  id: number;
+  stockId: number;
+  oldCount: number;
+  newCount: number;
+  action: string;
+  createdAt: Date;
+  user: UserDto;
+}
+
+class UserDto {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  isSchoolEmployee: boolean;
+}
