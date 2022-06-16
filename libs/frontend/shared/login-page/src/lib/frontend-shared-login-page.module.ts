24dcbc4e8e089aeb0152f80f0f46dfa5baa54aa6
Alan Schaeffer <alan.schaeffer29@gmail.com>
2022-06-16 17:31:45 +0200

    add(translation) Adding all the translating files and removing the last hardcoded string

--- libs/frontend/shared/login-page/src/lib/frontend-shared-login-page.module.ts
+++ libs/frontend/shared/login-page/src/lib/frontend-shared-login-page.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
+import { FrontendCoreI18nModule } from '@stud-asso/frontend/core/i18n';
 import { FrontendSharedModalModule } from '@stud-asso/frontend-shared-modal';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [CommonModule, FrontendSharedModalModule],
+  imports: [CommonModule, FrontendSharedModalModule, FrontendCoreI18nModule],
   declarations: [LoginPageComponent],
   exports: [LoginPageComponent],
 })
--- libs/frontend/shared/modal/src/lib/frontend-shared-modal.module.ts
+++ libs/frontend/shared/modal/src/lib/frontend-shared-modal.module.ts
@@ -2,11 +2,12 @@ import { BaseModalComponent } from './base-modal/base-modal.component';
 import { CommonModule } from '@angular/common';
 import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
 import { FormModalComponent } from './form-modal/form-modal.component';
+import { FrontendCoreI18nModule } from '@stud-asso/frontend/core/i18n';
 import { FrontendSharedFormlyModule } from '@stud-asso/frontend-shared-formly';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [CommonModule, FrontendSharedFormlyModule],
+  imports: [CommonModule, FrontendSharedFormlyModule, FrontendCoreI18nModule],
   declarations: [BaseModalComponent, FormModalComponent, ConfirmModalComponent],
 })
 export class FrontendSharedModalModule {}
