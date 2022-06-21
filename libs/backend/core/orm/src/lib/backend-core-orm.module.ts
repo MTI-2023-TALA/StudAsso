a9e187b6a07b45a5750b370a76ce400b5973e1d3
Loup Dallier <loup.dallier@epita.fr>
2022-06-21 11:59:44 +0200

    remove entities and base repository

--- libs/backend/core/orm/src/lib/backend-core-orm.module.ts
+++ libs/backend/core/orm/src/lib/backend-core-orm.module.ts
@@ -1,24 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 
-import { Association } from './association.entity';
-import { AssociationsMember } from './association-member.entity';
-import { Event } from './event.entity';
-import { NewsFeed } from './news-feed.entity';
 import { PrismaService } from './prisma.service';
-import { Role } from './role.entity';
-import { Stock } from './stock.entity';
-import { StockLogs } from './stock-logs.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user.entity';
 
 @Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([Association, AssociationsMember, Event, NewsFeed, Role, Stock, StockLogs, User])],
-  controllers: [],
   providers: [PrismaService],
-  exports: [
-    PrismaService,
-    TypeOrmModule.forFeature([Association, AssociationsMember, Event, NewsFeed, Role, Stock, StockLogs, User]),
-  ],
+  exports: [PrismaService],
 })
 export class BackendCoreOrmModule {}
