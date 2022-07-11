7535a2a35bf9c1600717152fa867867fcc1f12f1
Alois <alois.heloir@live.fr>
2022-07-11 20:57:34 +0200

    [Frontend] Student: Add asso list (#147)
    * feat(association-list-page): Add page
    
    * fix(test)

--- libs/frontend/feature/student/association-list-page/src/lib/association-list-page/association-list-page.component.ts
+++ libs/frontend/feature/student/association-list-page/src/lib/association-list-page/association-list-page.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { AssociationDto, AssociationWithPresidentDto } from '@stud-asso/shared/dtos';
+import { Component, OnInit } from '@angular/core';
+
+import { ApiAssociationService } from '@stud-asso/frontend-core-api';
 
 @Component({
   selector: 'stud-asso-association-list-page',
   templateUrl: './association-list-page.component.html',
   styleUrls: ['./association-list-page.component.scss'],
 })
-export class AssociationListPageComponent {}
+export class AssociationListPageComponent implements OnInit {
+  associationList: AssociationWithPresidentDto[] = [];
+
+  constructor(private api: ApiAssociationService) {}
+
+  ngOnInit() {
+    this.api.findAll().subscribe((associationList) => {
+      this.associationList = associationList;
+    });
+  }
+}
