2ca0b7c04ad53fdaaa16779cf52abbbd267934f3
Théo Casner <theo.casner@epita.fr>
2022-09-03 14:57:26 +0200

    add(error lib): error enum

---
+++ libs/backend/core/error/README.md
@@ -0,0 +1,7 @@
+# backend-core-error
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test backend-core-error` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/backend/core/error/src/lib/error.enum.ts
@@ -0,0 +1,16 @@
+export const enum ERROR {
+  ACCESS_DENIED = 'Access Denied',
+  ASSO_NAME_ALREADY_EXISTS = 'Association Name Is Already In Use',
+  ASSO_NOT_FOUND = 'Association Not Found',
+  BAD_STOCK_ACTION = 'Provided action has to be either: create, update or delete when creating a stock logs',
+  CANNOT_DELETE_ROLE = 'Cannot Delete Role',
+  CANNOT_UPDATE_ROLE = 'Cannot Update Role',
+  EMAIL_ALREADY_USED = 'Email Already Used',
+  EVENT_NOT_FOUND = 'Event Not Found',
+  INVALID_GOOGLE_TOKEN = 'Invalid Google Token',
+  NEWS_NOT_FOUND = 'News Not Found',
+  PRESIDENT_NOT_FOUND = 'President Not Found',
+  ROLE_NOT_FOUND = 'Role Not Found',
+  STOCK_NOT_FOUND = 'Stock Not Found',
+  USER_NOT_FOUND = 'User Not Found',
+}
