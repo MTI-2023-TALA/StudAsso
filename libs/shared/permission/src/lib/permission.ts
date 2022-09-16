5e583282cbc2b1272daf986bae20b7abcd31381b
Alois <alois.heloir@live.fr>
2022-09-16 21:19:11 +0200

    [Backend] Permission: Add missing permission (#175)
    * feat(permission): Add missing permission
    
    * feat(permission): Add missing permission

--- libs/shared/permission/src/lib/permission.ts
+++ libs/shared/permission/src/lib/permission.ts
@@ -3,6 +3,11 @@ import { PermissionColor } from './color';
 export enum PermissionId {
   STOCK_MANAGEMENT = 'STOCK_MANAGEMENT',
   STOCK_READ = 'STOCK_READ',
+  MEMBER_ADD = 'MEMBER_ADD',
+  MEMBER_REMOVE = 'MEMBER_REMOVE',
+  NEWS_MANAGEMENT = 'NEWS_MANAGEMENT',
+  EVENT_MANAGEMENT = 'EVENEMENT_MANAGEMENT',
+  ROLE_MANAGEMENT = 'ROLE_MANAGEMENT',
 }
 
 export interface IPermission {
@@ -28,5 +33,35 @@ export const permissions: Permissions = {
     name: 'Stock Read',
     color: PermissionColor.SECONDARY,
     description: 'Stock Read',
+  },
+  MEMBER_ADD: {
+    id: PermissionId.MEMBER_ADD,
+    name: 'Member Add',
+    color: PermissionColor.SUCCESS,
+    description: 'Member Add',
+  },
+  MEMBER_REMOVE: {
+    id: PermissionId.MEMBER_REMOVE,
+    name: 'Member Remove',
+    color: PermissionColor.DANGER,
+    description: 'Member Remove',
+  },
+  NEWS_MANAGEMENT: {
+    id: PermissionId.NEWS_MANAGEMENT,
+    name: 'News Management',
+    color: PermissionColor.PRIMARY,
+    description: 'News Management',
+  },
+  EVENT_MANAGEMENT: {
+    id: PermissionId.EVENT_MANAGEMENT,
+    name: 'Evenement Management',
+    color: PermissionColor.PRIMARY,
+    description: 'Evenement Management',
+  },
+  ROLE_MANAGEMENT: {
+    id: PermissionId.ROLE_MANAGEMENT,
+    name: 'Role Management',
+    color: PermissionColor.PRIMARY,
+    description: 'Role Management',
   },
 };
