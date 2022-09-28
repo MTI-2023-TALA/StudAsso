36db543822719317a0e6b0e1ce0db256ac81049f
Alan Schaeffer <alan.schaeffer29@gmail.com>
2022-09-28 15:32:15 +0200

    feat: offer page for association

---
+++ libs/frontend/feature/association/offer-page/README.md
@@ -0,0 +1,7 @@
+# frontend-feature-association-offer-page
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test frontend-feature-association-offer-page` to execute the unit tests.
---
+++ libs/frontend/feature/association/offer-page/src/lib/frontend-feature-association-offer-page.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { OfferPageComponent } from './offer-page/offer-page.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [OfferPageComponent],
+})
+export class FrontendFeatureAssociationOfferPageModule {}
