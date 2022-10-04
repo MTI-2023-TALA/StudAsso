77ac9bd6ca5ad95c5564c06aa16c5b017d4a036e
Alan Schaeffer <alan.schaeffer29@gmail.com>
2022-10-04 15:44:02 +0200

    fix: removed unused import

--- libs/frontend/feature/association/financement-page/src/lib/frontend-feature-association-financement-page.module.ts
+++ libs/frontend/feature/association/financement-page/src/lib/frontend-feature-association-financement-page.module.ts
@@ -1,7 +1,6 @@
-import { FrontendSharedGenericPageModule, GenericPageComponent } from '@stud-asso/frontend/shared/generic-page';
-
 import { CommonModule } from '@angular/common';
 import { FinancementPageComponent } from './financement-page/financement-page.component';
+import { FrontendSharedGenericPageModule } from '@stud-asso/frontend/shared/generic-page';
 import { NgModule } from '@angular/core';
 
 @NgModule({
