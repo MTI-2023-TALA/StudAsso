2e084f1fbbf78f1d80d40732f7fb5e2cd2f8f945
Théo Casner <theo.casner@epita.fr>
2022-10-16 19:02:14 +0200

    add: add pagination to findAssociationMembersWithRoles

--- libs/backend/feature/association/src/lib/associations.service.ts
+++ libs/backend/feature/association/src/lib/associations.service.ts
@@ -80,13 +80,16 @@ export class AssociationsService {
     };
   }
 
-  public async findAssociationMembersWithRoles(associationId: number): Promise<AssociationMemberWithRoleDto[]> {
+  public async findAssociationMembersWithRoles(
+    associationId: number,
+    query: QueryPaginationDto
+  ): Promise<AssociationMemberWithRoleDto[]> {
     const association = await this.associationRepository.findOne(associationId);
     if (!association) {
       throw new Error(ERROR.ASSO_NOT_FOUND);
     }
 
-    const membersWithRoles = await this.associationsMemberRepository.findAssociationMembersWithRoles(associationId);
+    const membersWithRoles = await this.associationsMemberRepository.findAssociationMembersWithRoles(associationId, query);
     return membersWithRoles.map((member) => ({
       userFullName: `${member.user.firstname} ${member.user.lastname}`,
       userEmail: member.user.email,
