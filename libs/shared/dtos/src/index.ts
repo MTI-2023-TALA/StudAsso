ec556121e99a2eecb9326101625294d58c8119aa
Théo Casner <theo.casner@epita.fr>
2022-10-16 18:29:55 +0200

    add: exports for model & dto

--- libs/shared/dtos/src/index.ts
+++ libs/shared/dtos/src/index.ts
@@ -7,6 +7,7 @@ export * from './lib/auth/google-auth.dto';
 export * from './lib/event/event.dto';
 export * from './lib/funding/funding.dto';
 export * from './lib/news/news.dto';
+export * from './lib/query/query.dto';
 export * from './lib/roles/role.dto';
 export * from './lib/stocks/stock.dto';
 export * from './lib/user/user.dto';
