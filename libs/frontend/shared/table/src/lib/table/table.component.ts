b97fb7a19d6a7b9c204b109d37a1b736459cc76c
Alois Heloir <alois.heloir@live.fr>
2022-10-17 12:12:06 +0200

    refacto(planning)

--- libs/frontend/shared/table/src/lib/table/table.component.ts
+++ libs/frontend/shared/table/src/lib/table/table.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, ContentChild, Input, TemplateRef } from '@angular/core';
+import { Component, ContentChild, Input, TemplateRef } from '@angular/core';
 
 import { TableConfiguration } from './table.model';
 
@@ -6,7 +6,7 @@ import { TableConfiguration } from './table.model';
   selector: 'stud-asso-table',
   templateUrl: './table.component.html',
 })
-export class TableComponent implements AfterContentInit {
+export class TableComponent {
   @Input() tableConfiguration: TableConfiguration;
   @Input() data: any[];
   @ContentChild('headers') headers: TemplateRef<any> | undefined;
@@ -16,10 +16,5 @@ export class TableComponent implements AfterContentInit {
 
   toggleDropdown() {
     this.isActionActive = !this.isActionActive;
-  }
-
-  ngAfterContentInit(): void {
-    console.log(this.headers);
-    console.log(this.rows);
   }
 }
