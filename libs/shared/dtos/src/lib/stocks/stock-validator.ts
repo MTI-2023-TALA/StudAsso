7824a37d4ff61144f1e99fabbb09d47a1bfc9627
Théo Casner <theo.casner@epita.fr>
2022-10-17 13:19:17 +0200

    fix: remove bad tolowercase

--- libs/shared/dtos/src/lib/stocks/stock-validator.ts
+++ libs/shared/dtos/src/lib/stocks/stock-validator.ts
@@ -5,7 +5,7 @@ import { SORT_STOCK } from './stock.dto';
 @ValidatorConstraint({ name: 'stock-sort', async: false })
 export class StockSortValidator implements ValidatorConstraintInterface {
   validate(text: string, args?: ValidationArguments): boolean {
-    return Object.values(SORT_STOCK).includes(text.toLowerCase() as SORT_STOCK);
+    return Object.values(SORT_STOCK).includes(text as SORT_STOCK);
   }
 
   defaultMessage?(args?: ValidationArguments): string {
