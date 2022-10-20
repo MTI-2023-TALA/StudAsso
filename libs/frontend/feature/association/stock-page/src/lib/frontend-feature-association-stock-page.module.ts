723e8c694934575dcf45c05fecdc6b568c5409b8
Alan Schaeffer <alan.schaeffer29@gmail.com>
2022-10-20 17:21:25 +0200

    feat: added logs stock page

--- libs/frontend/feature/association/stock-page/src/lib/frontend-feature-association-stock-page.module.ts
+++ libs/frontend/feature/association/stock-page/src/lib/frontend-feature-association-stock-page.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { FrontendSharedGenericPageModule } from '@stud-asso/frontend/shared/generic-page';
 import { FrontendSharedTableModule } from '@stud-asso/frontend-shared-table';
+import { FrontendSharedTagModule } from '@stud-asso/frontend-shared-tag';
 import { NgModule } from '@angular/core';
 import { StockLogsComponent } from './stock-logs/stock-logs.component';
 import { StockPageComponent } from './stock-page/stock-page.component';
 
 @NgModule({
-  imports: [CommonModule, FrontendSharedTableModule, FrontendSharedGenericPageModule],
+  imports: [CommonModule, FrontendSharedTableModule, FrontendSharedGenericPageModule, FrontendSharedTagModule],
   declarations: [StockPageComponent, StockLogsComponent],
 })
 export class FrontendFeatureAssociationStockPageModule {}
--- libs/frontend/feature/association/stock-page/src/lib/stock-logs/stock-logs.component.ts
+++ libs/frontend/feature/association/stock-page/src/lib/stock-logs/stock-logs.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ApiStockService } from '@stud-asso/frontend-core-api';
+import { StockLogWithUserDto } from '@stud-asso/shared/dtos';
 
 @Component({
   selector: 'stud-asso-stock-logs',
   templateUrl: './stock-logs.component.html',
+  styleUrls: ['./stock-logs.component.scss'],
 })
-export class StockLogsComponent {
-  constructor() {
-    return;
+export class StockLogsComponent implements OnInit {
+  logs: StockLogWithUserDto[];
+
+  constructor(private api: ApiStockService) {}
+
+  ngOnInit(): void {
+    this.reloadData();
+  }
+
+  reloadData() {
+    this.api.findAllAssoStockLog().subscribe((logs: StockLogWithUserDto[]) => {
+      this.logs = logs;
+    });
   }
 }
