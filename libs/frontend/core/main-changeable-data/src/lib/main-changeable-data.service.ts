079d0d20712bf4cc61fb21e39cabc5a6971217c8
Alois Heloir <alois.heloir@live.fr>
2022-10-25 18:33:58 +0200

    fix(association): Correctly display association name

--- libs/frontend/core/main-changeable-data/src/lib/main-changeable-data.service.ts
+++ libs/frontend/core/main-changeable-data/src/lib/main-changeable-data.service.ts
@@ -1,3 +1,5 @@
+import { LocalStorageHelper, LocalStorageKey } from '@stud-asso/frontend-core-storage';
+
 import { BehaviorSubject } from 'rxjs';
 import { Injectable } from '@angular/core';
 
@@ -7,5 +9,7 @@ export class MainChangeableDataService {
 
   constructor() {
     this.associationName$ = new BehaviorSubject('Association');
+    const currentAssoName = LocalStorageHelper.getData<string>(LocalStorageKey.ASSOCIATION_NAME);
+    if (currentAssoName) this.associationName$.next(currentAssoName);
   }
 }
