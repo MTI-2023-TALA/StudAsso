0bc4630ec38583ecb6f3a6e16fc27ce058f933dc
Alois Heloir <alois.heloir@live.fr>
2022-11-22 14:21:32 +0100

    move postintall to prepare

--- tools/scripts/prepare.sh
+++ tools/scripts/prepare.sh
@@ -2,4 +2,5 @@ if [ "$NODE_ENV" = "production" ]; then
   echo "Production enable"
 else
   husky install && npx ts-node ./tools/scripts/env-generator.ts
+  ngcc --properties es2020 browser module main
 fi
